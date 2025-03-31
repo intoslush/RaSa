debc11560655e9594823b818588580890e7fe96d
ZA <intoslush@gmail.com>
2025-03-31 20:37:46 +0800

    这个作为原版的rasa的存档,用来测试一下是否真的能达到.76的准确度

--- shell/cuhk-train.sh
+++
@@ -1,9 +0,0 @@
-#export CUDA_VISIBLE_DEVICES=0,1,2,3
-export CUDA_VISIBLE_DEVICES=0,2
-
-python -m torch.distributed.run --nproc_per_node=2 --rdzv_endpoint=127.0.0.1:29501 \
-Retrieval.py \
---config configs/PS_cuhk_pedes.yaml \
---output_dir output/cuhk-pedes/train \
---checkpoint ./data/ALBEF/ALBEF.pth \
---eval_mAP
--- shell/data_process.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python data_process.py --dataset_name "CUHK-PEDES" --dataset_root_dir "../dataset/CUHK-PEDES"
-#python data_process.py --dataset_name "ICFG-PEDES" --dataset_root_dir "../dataset/ICFG-PEDES"
-#python data_process.py --dataset_name "RSTPReid" --dataset_root_dir "../dataset/RSTPReid"
